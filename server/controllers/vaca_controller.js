e80c5df8054e5258f6bb05dbb1f815511b5a6591
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-08 22:17:39 -0600

    update get and create

--- server/controllers/vaca_controller.js
+++ server/controllers/vaca_controller.js
@@ -1,19 +1,31 @@
 let events = [
-    { title: 'new mexico'}
+    { title: 'Hawaii', id: 1 },
+    { title: 'Alaska', id: 2 }
 ]
 
 module.exports = {
     getEvent: (req, res) => {
         res.status(200).send(events)
     },
-    createEvent: (req, res) => {
-        
-        res.status(200).send(events)
-    },
-    findEventByTitle: (req, res) => {
-        const findTitle = events.filter((findEvent) =>{
+    getEventByTitle: (req, res) => {
+        const findTitle = events.filter((findEvent) => {
             return findEvent.title === +req.params.title
         })
         res.status(200).send(findTitle[0])
+    },
+    getEventById: (req, res) => {
+        const findId = events.filter((findEvent) => {
+            return findEvent.id === +req.params.id
+        })
+        res.status(200).send(findId[0])
+    },
+    createEvent: (req, res) => {
+        let id = events.[events.length - 1].id++
+        const newEvent = {
+            title: req.body.title,
+            id: id
+        }
+        events = [...events, newEvent]
+        res.status(200).send(events)
     },
 }
--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const vc = require('./controllers/vaca_controller')
 app.use(express.json())
 
 app.get('/api/events', vc.getEvent)
+app.get('/api/events', vc.getEventByTitle)
+app.get('/api/events', vc.getEventById)
 app.post('/api/events', vc.createEvent)
+
 
 const PORT = 3555
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)) 
